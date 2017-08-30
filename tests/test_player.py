9349553ad0701556c0a371c73ebacd8ffc4df04e
Tory Clasen <ToryClasen@Gmail.com>
2017-08-30 15:38:15 -0400

    Added unittest for drawing a card

--- tests/test_player.py
+++ tests/test_player.py
@@ -6,3 +6,8 @@ class TestPlayer(TestCase):
     def test_create_player(self):
         p = Player()
         self.assertTrue(isinstance(p, Player))
+
+    def test_draw_card(self):
+        p = Player()
+        c = p.draw()
+        self.assertTrue(isinstance(c, None))
