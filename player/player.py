699e4a4be5db4b64e7d09aa90a64ba5c211210b0
Tory Clasen <ToryClasen@Gmail.com>
2017-08-30 15:26:29 -0400

    Added helper functions for draw and discard

--- player/player.py
+++ player/player.py
@@ -1,3 +1,23 @@
 class Player(object):
     def __init__(self):
-        pass
+        self.hand = None
+        self.deck = None
+        self.discard = None
+        self.board = None
+
+    def draw(self):
+        if len(deck) <= 0:
+            raise Exception("Deck is empty, can't draw")
+
+        card = self.deck.draw()
+        self.hand.add(card)
+
+    def discard(self, card):
+        if card in self.hand:
+            self.hand.remove(card)
+        elif card in self.deck:
+            self.deck.remove(card)
+        elif card in self.board:
+            self.board.remove(card)
+
+        self.discard.add(card)
